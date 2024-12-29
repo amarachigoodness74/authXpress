cf76428e8b86717f27eefd3083ac43d665b68b38
amarachigoodness74 <amarachigoodness74@gmail.com>
2024-12-29 20:52:16 +0100

    updated now

--- backend/src/utils/corsOptions.ts
+++ backend/src/utils/corsOptions.ts
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5000",
   "http://localhost:5000",
   "http://localhost:3000",
+  "https://auth-xpress.vercel.app",
 ];
 
 const corsOptions: CorsOptions = {
